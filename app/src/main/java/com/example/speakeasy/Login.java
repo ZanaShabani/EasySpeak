8b9652965bfb58d3136bc21dc18a14d515f9d289
Zana Shabani <zana.shabani2@student.uni-pr.edu>
2024-12-28 17:23:42 +0100

    Custom button added

--- app/src/main/java/com/example/speakeasy/Login.java
+++ app/src/main/java/com/example/speakeasy/Login.java
@@ -1,13 +1,23 @@
 package com.example.speakeasy;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Login extends AppCompatActivity {
+    Button signup_button;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        signup_button = findViewById(R.id.signup_button);
+
+        signup_button.setOnClickListener(view->{
+            startActivity(new Intent(this, Signup.class));
+        });
+
     }
 
 }
--- app/src/main/java/com/example/speakeasy/Signup.java
+++ app/src/main/java/com/example/speakeasy/Signup.java
@@ -1,12 +1,26 @@
 package com.example.speakeasy;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Signup extends AppCompatActivity {
+    TextView login_button;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        login_button = findViewById(R.id.login_button);
+
+
+        login_button.setOnClickListener(view->{
+            startActivity(new Intent(this, Login.class));
+        });
+
+
     }
 }
