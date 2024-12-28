4d1fb6d53c001fdd0dcf370dd1fd03dc0991b64c
ZanaShabani <zana.shabani2@student.uni-pr.edu>
2024-12-28 16:29:20 +0100

    Created Signup, Login, and Verify pages with layouts.

---
+++ app/src/main/java/com/example/speakeasy/Login.java
@@ -0,0 +1,13 @@
+package com.example.speakeasy;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Login extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+    }
+
+}
---
+++ app/src/main/java/com/example/speakeasy/Verify.java
@@ -0,0 +1,14 @@
+package com.example.speakeasy;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Verify extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.verify);
+}
+
+}
