8ccc03b4420344295ae0b6f67e2ae0f8861f4ce5
ZanaShabani <zana.shabani2@student.uni-pr.edu>
2024-12-30 12:09:02 +0100

    Add password reset functionality.

---
+++ app/src/main/java/com/example/speakeasy/ResetPassword.java
@@ -0,0 +1,14 @@
+package com.example.speakeasy;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ResetPassword extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reset_password);
+
+
+    }
+}
